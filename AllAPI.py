42bcb31946d4b835cf5459c66f8f7d109d65174c
Prashikshit Saini <111597081+PrashikshitSaini@users.noreply.github.com>
2024-01-17 11:23:54 +0530

    Update AllAPI.py

--- AllAPI.py
+++ AllAPI.py
@@ -83,7 +83,7 @@ def display_idioms(idioms, entry_number):
 
 
 def give_it_out() -> str:
-    i = random.randint(1, 128)
+    i = random.randint(25, 128)
     output_buffer = io.StringIO()
 
     try:
